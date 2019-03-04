a55f2c7878d8c0a530ec9082a78a32588076a1cd
Emily Nuri <emily.nuri4@gmail.com>
2019-03-04 14:38:20 -0800

    Updates

--- precache-manifest.4278a8e3bff7426dfe5fe739d98f3ad6.js
+++ precache-manifest.4278a8e3bff7426dfe5fe739d98f3ad6.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/seattlefoodbankfinder/static/js/runtime~main.4387aa3d.js"
   },
   {
-    "revision": "2e140c32723b5705c7b1",
-    "url": "/seattlefoodbankfinder/static/js/main.2e140c32.chunk.js"
+    "revision": "5d76d28061ff7fc7f0c3",
+    "url": "/seattlefoodbankfinder/static/js/main.5d76d280.chunk.js"
   },
   {
     "revision": "a231bc35e2c780f2abbd",
     "url": "/seattlefoodbankfinder/static/js/2.a231bc35.chunk.js"
   },
   {
-    "revision": "2e140c32723b5705c7b1",
+    "revision": "5d76d28061ff7fc7f0c3",
     "url": "/seattlefoodbankfinder/static/css/main.0f493486.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/seattlefoodbankfinder/static/css/2.5fa23089.chunk.css"
   },
   {
-    "revision": "2c21a8043a9fafd882fdfaf42c6a120f",
+    "revision": "7d2868d7945550480feaf3c8c42bf165",
     "url": "/seattlefoodbankfinder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/seattlefoodbankfinder/precache-manifest.d024defe3667d2e44aaf19268e562c1e.js"
+  "/seattlefoodbankfinder/precache-manifest.4278a8e3bff7426dfe5fe739d98f3ad6.js"
 );
 
 workbox.clientsClaim();
